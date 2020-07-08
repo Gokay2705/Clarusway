36097ee0405590ed3ad096a6b065786c0c76b164
E 2193 <63980772+Gokay2705@users.noreply.github.com>
2020-07-08 19:08:10 +0200

    Add files via upload
    Assigment 1

---
+++ Assignment - 1 (if-Statements).py
@@ -0,0 +1,19 @@
+# To add a new cell, type '# %%'
+# To add a new markdown cell, type '# %% [markdown]'
+# %%
+name = input('insert your name : ').strip().title() == 'Mustafa'
+
+# print(name)
+
+if name:
+
+    print("Hello, Mustafa The password is : W@12")
+
+else:
+
+    print('Hello, Amina! See you later.')
+
+
+# %%
+
+
